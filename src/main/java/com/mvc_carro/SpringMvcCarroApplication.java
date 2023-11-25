6bbe41d6556f7ddfecc430c0806e04adc8df15e1
L-edu <edurei75@gmail.com>
2023-11-25 18:15:35 -0300

    first commit

---
+++ src/main/java/com/mvc_carro/SpringMvcCarroApplication.java
@@ -0,0 +1,13 @@
+package com.mvc_carro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcCarroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcCarroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mvc_carro/SpringMvcCarroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvc_carro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcCarroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
